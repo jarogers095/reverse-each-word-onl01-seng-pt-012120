cf5a11e1fbc56dc70ec7447ebac7497bc40534df
James Rogers <jarogers095@gmail.com>
2020-01-27 07:51:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,11 @@ def reverse_each_word(sentence)
   word_array = sentence.split(" ")
   
   
-  word_array.collect do |word|
+  reverse_word_array = word_array.collect do |word|
     word.reverse
   end
   
+  reverse_sentence = reverse_word_array.join(" ")
   
+  return reverse_sentence
 end
